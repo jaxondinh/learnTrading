baced6fa738c940e002129e263b5e46972026fa2
Jackson Dinh <jacksondinh@Jacksons-MacBook-Air.local>
2023-12-11 17:29:24 +1100

    up to rising three complete

--- src/imageMapping.js
+++ src/imageMapping.js
@@ -7,6 +7,9 @@ import TriImage from './images/Tri.png';
 import BearishHaramiImage from './images/Bearish-Harami.png';
 import BullishHaramiImage from './images/Bullish-Harami.png';
 import HaramiCrossImage from './images/Harami-Cross.png';
+import BullishEngulfingImage from './images/Bullish-Engulfing.png';
+import BearishEngulfingImage from './images/Bearish-Engulfing.png';
+import RisingThreeImage from './images/Rising-Three.png';
 export const imageMap = {
     "Doji": DojiImage,
     "The Shooting Star": ShootingStarImage,
@@ -16,5 +19,8 @@ export const imageMap = {
     "The Tri Star": TriImage,
     "The Bearish Harami": BearishHaramiImage,
     "The Bullish Harami": BullishHaramiImage,
-    "The Harami Cross": HaramiCrossImage
+    "The Harami Cross": HaramiCrossImage,
+    "The Bullish Engulfing Pattern": BullishEngulfingImage,
+    "The Bearish Engulfing Pattern": BearishEngulfingImage,
+    "The Rising Three Method": RisingThreeImage
 };
